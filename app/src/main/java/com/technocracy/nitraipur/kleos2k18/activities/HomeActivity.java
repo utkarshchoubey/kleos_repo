243d3c6cc431c4393dbe927fe06bf2030d5d07f9
Shritesh Jamulkar <shritesh.sj@gmail.com>
2018-08-25 14:39:21 +0530

    Initial commit

---
+++ app/src/main/java/com/technocracy/nitraipur/kleos2k18/activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.technocracy.nitraipur.kleos2k18.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.technocracy.nitraipur.kleos2k18.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
