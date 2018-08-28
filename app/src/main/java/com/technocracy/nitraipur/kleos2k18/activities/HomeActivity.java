142b76effa98e8cf9ee96ec7723d0309f577c4f0
Shritesh Jamulkar <shritesh.sj@gmail.com>
2018-08-28 15:48:11 +0530

    UI Improvements

--- app/src/main/java/com/technocracy/nitraipur/kleos2k18/activities/HomeActivity.java
+++ app/src/main/java/com/technocracy/nitraipur/kleos2k18/activities/HomeActivity.java
@@ -1,26 +1,37 @@
 package com.technocracy.nitraipur.kleos2k18.activities;
 
+import android.app.ActivityManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.technocracy.nitraipur.kleos2k18.R;
 import com.yarolegovich.slidingrootnav.SlidingRootNav;
 import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
+import am.appwise.components.ni.NoInternetDialog;
+
 public class HomeActivity extends AppCompatActivity {
     private SlidingRootNav slidingRootNav;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        TextView questionToolbar = findViewById(R.id.question_textview);
 
         slidingRootNav = new SlidingRootNavBuilder(this)
                 .withToolbarMenuToggle(toolbar)
