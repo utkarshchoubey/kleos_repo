b86f18eeeaafdbc6059f0f52dd21d62a6be616f4
utkarshchoubey <utkarshchoubey629@gmail.com>
2018-09-10 23:44:59 +0530

    pages updated

--- app/src/main/java/com/technocracy/nitraipur/kleos2k18/activities/SponsorsActivity.java
+++ app/src/main/java/com/technocracy/nitraipur/kleos2k18/activities/SponsorsActivity.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 
 import com.technocracy.nitraipur.kleos2k18.R;
 
+import static maes.tech.intentanim.CustomIntent.customType;
+
 public class SponsorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sponsors);
+
+        customType(this, "fadein-to-fadeout");
+
     }
 }
--- app/src/main/java/com/technocracy/nitraipur/kleos2k18/activities/StoryLineActivity.java
+++ app/src/main/java/com/technocracy/nitraipur/kleos2k18/activities/StoryLineActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 
 import com.technocracy.nitraipur.kleos2k18.R;
 
+import static maes.tech.intentanim.CustomIntent.customType;
+
 public class StoryLineActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_line);
+        customType(this, "fadein-to-fadeout");
+
     }
 }
