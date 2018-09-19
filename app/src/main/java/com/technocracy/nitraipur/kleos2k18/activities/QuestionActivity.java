480b243b63bb05be4c67940fc94e02ab9e3d7066
Shritesh Jamulkar <shritesh.sj@gmail.com>
2018-09-20 02:26:47 +0530

    Final changes

--- app/src/main/java/com/technocracy/nitraipur/kleos2k18/activities/QuestionActivity.java
+++ app/src/main/java/com/technocracy/nitraipur/kleos2k18/activities/QuestionActivity.java
@@ -21,6 +21,10 @@ import android.widget.VideoView;
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 import com.github.florent37.viewtooltip.ViewTooltip;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.technocracy.nitraipur.kleos2k18.R;
 import com.technocracy.nitraipur.kleos2k18.models.Question;
 import com.technocracy.nitraipur.kleos2k18.models.User;
@@ -37,18 +41,19 @@ import retrofit2.Response;
 
 import static maes.tech.intentanim.CustomIntent.customType;
 
-public class QuestionActivity extends AppCompatActivity {
-    TextView tv,tv1,textView1,textView2,textView3,textView4,textView5;
+public class QuestionActivity extends YouTubeBaseActivity {
+    TextView tv, tv1, textView1, textView2, textView3, textView4, textView5;
     UserPreferences userPreferences;
     EditText ed;
     TextInputLayout til;
     ApiEndpoints apiBase;
     ImageZoomHelper imageZoomHelper;
-    Button button,dialer;
+    Button button, dialer;
     Question q;
-    VideoView videoView;
-    ImageView img,img2;
+    YouTubePlayerView videoView;
+    ImageView img, img2;
     int pos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,19 +61,19 @@ public class QuestionActivity extends AppCompatActivity {
         customType(this, "fadein-to-fadeout");
         userPreferences = new UserPreferences(this);
 
-        img = (ImageView)findViewById(R.id.img);
-        img2 = (ImageView)findViewById(R.id.img2);
+        img = (ImageView) findViewById(R.id.img);
+        img2 = (ImageView) findViewById(R.id.img2);
         imageZoomHelper = new ImageZoomHelper(this);
         ImageZoomHelper.setViewZoomable(findViewById(R.id.img));
         ImageZoomHelper.setViewZoomable(findViewById(R.id.img2));
-        videoView=(VideoView)findViewById(R.id.video);
-        tv=(TextView)findViewById(R.id.questionText);
-        tv1=(TextView)findViewById(R.id.kleos);
-        til = (TextInputLayout)findViewById(R.id.til);
-        ed=(EditText)findViewById(R.id.answer);
-        LinearLayout linearLayout =(LinearLayout)findViewById(R.id.ques1);
-        button=(Button)findViewById(R.id.submitB);
-        dialer=(Button)findViewById(R.id.dialer);
+        videoView = (YouTubePlayerView) findViewById(R.id.video);
+        tv = (TextView) findViewById(R.id.questionText);
+        tv1 = (TextView) findViewById(R.id.kleos);
+        til = (TextInputLayout) findViewById(R.id.til);
+        ed = (EditText) findViewById(R.id.answer);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ques1);
+        button = (Button) findViewById(R.id.submitB);
+        dialer = (Button) findViewById(R.id.dialer);
         Slice slice = new Slice(button);
         slice.setRadius(8f);
         slice.setColor(Color.parseColor("#00BB84"));
@@ -76,18 +81,17 @@ public class QuestionActivity extends AppCompatActivity {
         slice2.setRadius(8f);
         slice2.setColor(Color.parseColor("#00BB84"));
 
-        textView1=(TextView)findViewById(R.id.t1);
-        textView2=(TextView)findViewById(R.id.t2);
-        textView3=(TextView)findViewById(R.id.t3);
-        textView4=(TextView)findViewById(R.id.t4);
-        textView5=(TextView)findViewById(R.id.t5);
-
+        textView1 = (TextView) findViewById(R.id.t1);
+        textView2 = (TextView) findViewById(R.id.t2);
+        textView3 = (TextView) findViewById(R.id.t3);
+        textView4 = (TextView) findViewById(R.id.t4);
+        textView5 = (TextView) findViewById(R.id.t5);
 
 
         q = getIntent().getParcelableExtra("question");
         pos = getIntent().getExtras().getInt("id");
 
-        switch(pos){
+        switch (pos) {
             case 1:
                 videoView.setVisibility(View.GONE);
                 videoView.setEnabled(false);
@@ -154,7 +158,7 @@ public class QuestionActivity extends AppCompatActivity {
                 dialer.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent intent=new Intent(QuestionActivity.this,question6_dialer.class);
+                        Intent intent = new Intent(QuestionActivity.this, question6_dialer.class);
                         startActivity(intent);
                     }
                 });
@@ -179,21 +183,18 @@ public class QuestionActivity extends AppCompatActivity {
                 img2.setEnabled(false);
                 dialer.setVisibility(View.GONE);
                 dialer.setEnabled(false);
-                String path = "android.resource://" + getPackageName() + "/" + R.raw.q8;
-                videoView.setVideoURI(Uri.parse(path));
-                videoView.start();
+                playVideo("a2ehVHdEsWw", videoView);
                 break;
             case 9:
                 img.setVisibility(View.GONE);
-                img.setEnabled(false);linearLayout.setVisibility(View.GONE);
+                img.setEnabled(false);
+                linearLayout.setVisibility(View.GONE);
                 linearLayout.setEnabled(false);
                 img2.setVisibility(View.GONE);
                 img2.setEnabled(false);
                 dialer.setVisibility(View.GONE);
                 dialer.setEnabled(false);
-                String path1 = "android.resource://" + getPackageName() + "/" + R.raw.q9;
-                videoView.setVideoURI(Uri.parse(path1));
-                videoView.start();
+                playVideo("HIStRZM1jMM", videoView);
                 break;
             case 10:
                 videoView.setVisibility(View.GONE);
@@ -236,7 +237,7 @@ public class QuestionActivity extends AppCompatActivity {
 
         int level = Integer.parseInt(userPreferences.getLevel());
 
-        if(pos <= level){
+        if (pos <= level) {
             button.setVisibility(View.GONE);
             ed.setVisibility(View.GONE);
             til.setVisibility(View.GONE);
@@ -248,40 +249,31 @@ public class QuestionActivity extends AppCompatActivity {
         apiBase = ApiBase.getClient().create(ApiEndpoints.class);
     }
 
-    Handler h2=new Handler();
-    Runnable r2=new Runnable() {
+    Handler h2 = new Handler();
+    Runnable r2 = new Runnable() {
         @Override
         public void run() {
-            switch(pos) {
-                case 1: int curBrightnessValue = android.provider.Settings.System.getInt(getContentResolver(), android.provider.Settings.System.SCREEN_BRIGHTNESS, -1);
-                        if (curBrightnessValue < 130) {
-                    textView1.setText("G");
-                    textView2.setText("");
-                    textView3.setText("B");
-                    textView4.setText("");
-                    textView5.setText("E");
-                      } else {
-                    textView1.setText("");
-                    textView2.setText("C");
-                    textView3.setText("");
-                    textView4.setText("F");
-                    textView5.setText("");
-                     }break;
-                case 8:if(!videoView.isPlaying()){
-                    String path = "android.resource://" + getPackageName() + "/" + R.raw.q8;
-                    videoView.setVideoURI(Uri.parse(path));
-                    videoView.start();
-                }
+            switch (pos) {
+                case 1:
+                    int curBrightnessValue = android.provider.Settings.System.getInt(getContentResolver(), android.provider.Settings.System.SCREEN_BRIGHTNESS, -1);
+                    if (curBrightnessValue < 130) {
+                        textView1.setText("G");
+                        textView2.setText("");
+                        textView3.setText("B");
+                        textView4.setText("");
+                        textView5.setText("E");
+                    } else {
+                        textView1.setText("");
+                        textView2.setText("C");
+                        textView3.setText("");
+                        textView4.setText("F");
+                        textView5.setText("");
+                    }
                     break;
-                case 9:if(!videoView.isPlaying()){
-                    String path = "android.resource://" + getPackageName() + "/" + R.raw.q9;
-                    videoView.setVideoURI(Uri.parse(path));
-                    videoView.start();
-                }
-                    break;
-                default: h2.removeCallbacks(r2);
+                default:
+                    h2.removeCallbacks(r2);
             }
-            h2.postDelayed(r2,500);
+            h2.postDelayed(r2, 500);
         }
     };
 
@@ -294,7 +286,7 @@ public class QuestionActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        h2.postDelayed(r2,500);
+        h2.postDelayed(r2, 500);
     }
 
     public void backToHomeActivity(View view) {
@@ -302,7 +294,7 @@ public class QuestionActivity extends AppCompatActivity {
     }
 
     public void submitAnswer(View view) {
-        if(ed.getText().toString().length() > 0){
+        if (ed.getText().toString().length() > 0) {
             YoYo.with(Techniques.FadeOut).duration(500).playOn(view);
             runOnUiThread(new Runnable() {
                 @Override
@@ -317,12 +309,12 @@ public class QuestionActivity extends AppCompatActivity {
 
                 }
             });
-            Call<User> call = apiBase.submitAnswer(userPreferences.getUsername(),String.valueOf(pos),String.valueOf(ed.getText()));
+            Call<User> call = apiBase.submitAnswer(userPreferences.getUsername(), String.valueOf(pos), String.valueOf(ed.getText()));
             call.enqueue(new Callback<User>() {
                 @Override
                 public void onResponse(@NonNull Call<User> call, @NonNull Response<User> response) {
-                    if(response.isSuccessful()){
-                        if(String.valueOf(response.body().message).equals("Congratulations your answer is correct")){
+                    if (response.isSuccessful()) {
+                        if (String.valueOf(response.body().message).equals("Congratulations your answer is correct")) {
                             userPreferences.setLevel(String.valueOf(pos));
                             Toasty.success(QuestionActivity.this, "Congratulations your answer is correct!!", Toast.LENGTH_SHORT, true).show();
                             runOnUiThread(new Runnable() {
@@ -332,14 +324,13 @@ public class QuestionActivity extends AppCompatActivity {
                                     handler.postDelayed(new Runnable() {
                                         @Override
                                         public void run() {
-                                           finish();
+                                            finish();
                                         }
                                     }, 1000);
 
                                 }
                             });
-                        }
-                        else{
+                        } else {
                             Toasty.error(QuestionActivity.this, "Sorry Wrong Answer.Please Try Again!!", Toast.LENGTH_SHORT, true).show();
                             YoYo.with(Techniques.FadeIn).duration(500).playOn(view);
                             button.setVisibility(View.VISIBLE);
@@ -356,14 +347,14 @@ public class QuestionActivity extends AppCompatActivity {
             });
 
 
-        }else{
+        } else {
             ViewTooltip.on(view)
                     .align(ViewTooltip.ALIGN.CENTER)
                     .position(ViewTooltip.Position.TOP)
                     .text("This Field Can't be Empty.")
                     .textColor(Color.WHITE)
                     .color(Color.parseColor("#00BB84"))
-                    .padding(2,2,2,2)
+                    .padding(2, 2, 2, 2)
                     .corner(15)
                     .arrowWidth(15)
                     .arrowHeight(15)
@@ -372,8 +363,26 @@ public class QuestionActivity extends AppCompatActivity {
             return;
         }
     }
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         return imageZoomHelper.onDispatchTouchEvent(ev) || super.dispatchTouchEvent(ev);
+    }
+
+    public void playVideo(final String videoId, YouTubePlayerView youTubePlayerView) {
+        youTubePlayerView.initialize("AIzaSyCU1NnGxmtqRvM7qJu8XM4d3JxarXbDfzY",
+                new YouTubePlayer.OnInitializedListener() {
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                        YouTubePlayer youTubePlayer, boolean b) {
+                        youTubePlayer.cueVideo(videoId);
+                    }
+
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                        YouTubeInitializationResult youTubeInitializationResult) {
+
+                    }
+                });
     }
 }
